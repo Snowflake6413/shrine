762800b9dd825d268064c4d7df40402c7aac8edc
Snowflake6413 <verycoolandsmart@protonmail.com>
2026-01-10 23:42:38 -0500

    the foundation layer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<title>Shrine</title>
+<h1>Shrine</h1>
+<!---TBA because we dont know.....goog-->
+<p>Make and code a project about your favorite fictional character, get a grant to buy merch of your character!</p>
+
+<div class="container">
+<a href="https://goog.fillout.com/shrine-ysws-rsvp">
+    <button>RSVP!</button>
+</a>
+ </div>
+
+<div>
+<details>
+    <summary>Q: So, what is Shrine?</summary>
+    <p>A: Shrine is a YSWS where you build a project about your favorite fictional character! It could be anything! Like, a website about your character, a Visual Novel with RenPy, anything related to that character! After you submit and we check your project, you'll recieve a grant to go buy merch of your favorite character!</p>
+</details>
+<details>
+    <summary>Q: How do I join?</summary>
+    <p>Join us at #shrine and make sure you RSVP on the form above!</p>
+</details>
+<details>
+    <summary>Q: When will this launch?</summary>
+    <p>We dont have an ETA right now since this YSWS draft is freshly new and we haven't found a sponsor yet!</p>
+</details>
+<details>
+    <summary>Q: When will this launch?</summary>
+    <p>We dont have an ETA right now since this YSWS draft is freshly new and we haven't found a sponsor yet!</p>
+</details>
+<details>
+    <summary>Q: How many hours do I have to work?</summary>
+    <p>We haven't set how many hours. It might be 5 or 6 hours. Although, this might change, please take it with a grain of salt!</p>
+</details>
+</div>
+
+
+<div>
+    <h3>Contacts</h3>
+    <p>You can contact the org via slack or email!</p>
+    <a href="https://hackclub.enterprise.slack.com/team/U09PHG7RLGG">
+        <p>Slack</p>
+    </a>
+    <a href="mailto:me@ifeellikeharukananase.tech">
+        <p>Email</p>
+    </a>
+</div>
+
+
